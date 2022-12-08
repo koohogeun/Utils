41f62ad7c48f009f761e6b02d4b9b62497d40e69
koohogeun <47141350+koohogeun@users.noreply.github.com>
2022-12-08 17:29:32 +0900

    Create gpu_free_mem.py

---
+++ GPU/gpu_free_mem.py
@@ -0,0 +1,25 @@
+import subprocess
+
+DEFAULT_ATTRIBUTES = (
+    'index',
+    'uuid',
+    'name',
+    'timestamp',
+    'memory.total',
+    'memory.free',
+    'memory.used',
+    'utilization.gpu',
+    'utilization.memory'
+)
+
+def get_free_gpu_mem_info(nvidia_smi_path='nvidia-smi', keys=DEFAULT_ATTRIBUTES, no_units=True):
+    nu_opt = '' if not no_units else ',nounits'
+    cmd = '%s --query-gpu=%s --format=csv,noheader%s' % (nvidia_smi_path, ','.join(keys), nu_opt)
+    output = subprocess.check_output(cmd, shell=True)
+    lines = output.decode().split('\n')
+    lines = [ line.strip() for line in lines if line.strip() != '' ]
+    infos = [ { k: v for k, v in zip(keys, line.split(', ')) } for line in lines ]
+    gpus_mem_info = [info['memory.free'] for info in infos]
+    return gpus_mem_info
+
+print(get_free_gpu_mem_info())
